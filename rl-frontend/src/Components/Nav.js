58778a099b497f8c30b4bcbf55e1d7dd4f844472
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-07-23 13:38:23 -0700

    running lg

--- rl-frontend/src/Components/Nav.js
+++ rl-frontend/src/Components/Nav.js
@@ -61,8 +61,7 @@ function App() {
       <Nav profile={profile} />
       <section className="hero-section">
         <div className="container d-flex align-items-center justify-content-center fs-1 text-white flex-column">
-          <h1>Responsive NavBar</h1>
-          <h2>Bootstrap 5</h2>
+          <h1>Running Logistics</h1>
         </div>
       </section>
     </>
