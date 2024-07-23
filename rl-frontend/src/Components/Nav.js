c16914e0aa234c7c8ed4529b66ddcce67479ed37
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-07-23 16:46:23 -0700

    removed hero section

--- rl-frontend/src/Components/Nav.js
+++ rl-frontend/src/Components/Nav.js
@@ -53,21 +53,21 @@ function Nav({ profile }) {
   );
 }
 
-function App() {
-  const profile = '';
+// function App() {
+//   const profile = '';
 
-  return (
-    <>
-      <Nav profile={profile} />
-      <section className="hero-section">
-        <div className="container d-flex align-items-center justify-content-center fs-1 text-white flex-column">
-          <h1>Running Logistics</h1>
-        </div>
-      </section>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Nav profile={profile} />
+//       <section className="hero-section">
+//         <div className="container d-flex align-items-center justify-content-center fs-1 text-white flex-column">
+//           <h1>Running Logistics</h1>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
 
-export default App;
+export default Nav;
 
 //container-fluid in above div to make it spaced out
