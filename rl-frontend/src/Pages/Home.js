39c19302b542bfea50f532a1b8f10c1e8012de77
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-07-23 14:03:09 -0700

    services and subscribe sec

--- rl-frontend/src/Pages/Home.js
+++ rl-frontend/src/Pages/Home.js
@@ -5,10 +5,36 @@ import '../CSS/Home.css'
 
 function Home() {
   return (
-    <section id='home'>
+    <div id='home'>
+      {/* Services Section */}
+      <section className='services-section'>
+        <h2>Services</h2>
+        <div className='services-container'>
+          <div className='service'>
+            <img src='#' alt='Service 1' className='service-image' />
+            <p className='service-text'>Service 1</p>
+          </div>
+          <div className='service'>
+            <img src='#' alt='Service 2' className='service-image' />
+            <p className='service-text'>Service 2</p>
+          </div>
+          <div className='service'>
+            <img src='#' alt='Service 3' className='service-image' />
+            <p className='service-text'>Service 3</p>
+          </div>
+        </div>
+      </section>
 
-    </ section>
-  )
+      {/* Subscribe Section */}
+      <section className='subscribe-section'>
+        <h2>Subscribe</h2>
+        <form className='subscribe-form'>
+          <input type='text' placeholder='Enter your email' className='subscribe-input' />
+          <button type='submit' className='subscribe-button'>Sign Up</button>
+        </form>
+      </section>
+    </div>
+  );
 }
 
 export default Home
